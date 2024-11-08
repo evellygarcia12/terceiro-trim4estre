2f4995d57143fea112b91a0721686eceb4b4bfbc
evellygarcia12 <161785811+evellygarcia12@users.noreply.github.com>
2024-11-07 22:33:09 -0300

    Add files via upload

---
+++ trerceiro trimestre/perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+      'Lingua Portuguesa',
+      'O que e verbo?',
+      'Classe gramatical que exprime,principalmente,ações,estados e fenômenos'
+)
+     
+criaCartao( 
+       'Geo', 
+       'Oque é o Sol?',
+       'Uma estrela'  
+)
+
+criaCartao(
+       'Geo', 
+       'Oque a lua é',
+       'Um satelitess'
+)
+
